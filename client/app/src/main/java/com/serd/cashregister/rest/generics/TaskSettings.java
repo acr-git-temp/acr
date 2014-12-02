9d94711e7e17ce40294a004b4ab713c31bf73dae
franktom <tomas.frankl@gmail.com>
2014-12-02 21:20:09 +0100

    Presunuty soubor TaskSettings

--- client/app/src/main/java/com/serd/cashregister/rest/generics/TaskSettings.java
+++
@@ -1,9 +0,0 @@
-package com.serd.cashregister.rest.generics;
-
-/**
- * Created by Tomas on 18. 11. 2014.
- */
-public class TaskSettings {
-    public final static String baseurl = "http://192.168.1.107:8000/";
-    //final static String baseurl = "http://127.0.0.1:8000/";
-}
